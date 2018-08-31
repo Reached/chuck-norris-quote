77dd2475a8799d7962bd59bc40dd90633cbbc0b8
Casper Sørensen <casper@ulobby.dk>
2018-08-31 20:51:57 +0200

    Started building the homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,43 @@
 <template>
-  <Page class="page">
-    <ActionBar class="action-bar" title="Home"/>
+  <Page class="page" backgroundSpanUnderStatusBar="true">
+    <ActionBar flat="true" class="action-bar" title="">
+      <ActionItem text="Categories" ios.position="left" icon="res://selection"></ActionItem>
+      <ActionItem text="Settings" ios.position="right" icon="res://ic_cog"></ActionItem>
+    </ActionBar>
 
-    <StackLayout>
-      <Button class="btn btn-primary" @tap="$router.push('/counter')">Counter</Button>
-      <Button class="btn btn-primary" @tap="$router.push('/hello')">Hello World</Button>
-    </StackLayout>
-
+      <StackLayout clas="joke-box">
+        <Button @tap="getRandomJoke">Get a new joke</Button>
+        <Image src="~/chuck-on-fire.png" stretch="none"></Image>
+        <Label class="joke-text" v-if="joke" :textWrap="true" :text="joke.value"></Label>
+      </StackLayout>
   </Page>
 </template>
+
+<script>
+  import Settings from '../Settings';
+
+  export default {
+      data() {
+          return {
+              joke: null
+          }
+      },
+      created() {
+          this.getRandomJoke();
+      },
+      methods: {
+          getRandomJoke() {
+              fetch(`${Settings.BASE_URL}/jokes/random`, {
+                  method: 'GET'
+              }).then((result) => {
+                  return result.json();
+              }).then((response) => {
+                  this.joke = response;
+                  console.log(response);
+              }).catch((error) => {
+                  console.log(error);
+              });
+          }
+      }
+  }
+</script>
